ca886b1124878ac2b8e1b4f8e9ff16eaa3e2e2e9
Dmytro-Hrebenyuk <149362391+Dmytro-Hrebenyuk@users.noreply.github.com>
2024-11-03 16:45:51 +0200

    Update PostDeliveryStrategy.java

--- src/main/java/flower/store/PostDeliveryStrategy.java
+++ src/main/java/flower/store/PostDeliveryStrategy.java
@@ -1,7 +1,38 @@
 package flower.store;
-public class PayPalPaymentStrategy implements Payment {
+
+import java.util.List;
+
+public class PostDeliveryStrategy implements Delivery {
+    private String adress;
+    private  Order order;
     @Override
-    public String pay(double price) {
-        return String.format("Paid %.2f with credit card", price);
+    public String deliver(List<Flower> flowers) {
+        return "Flowers delivered via post: " + flowers.size() + " items.";
     }
+
+    @Override
+    public Order getOrder() {
+        return order;
+    }
+
+    @Override
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public String getAddress() {
+        return this.adress;
+    }
+
+    @Override
+    public void setAddress(String address) {
+        this.adress = address;
+    }
+
+    @Override
+    public String getdelivery() {
+        return "Post Delivery";
+    }
+    
 }
