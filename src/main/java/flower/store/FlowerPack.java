8b18c10156dc1baaf11a595a81b7ebe53aa3a7df
Dmytro-Hrebenyuk <149362391+Dmytro-Hrebenyuk@users.noreply.github.com>
2024-11-03 16:43:28 +0200

    Update FlowerPack.java

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,6 +1,15 @@
 package flower.store;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class FlowerPack {
     private Flower flower;
-    private int quantity;
+    private int amount;
+
+    public double getPrice() {
+        return flower.getPrice() * amount;
+    }
 }
