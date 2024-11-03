802d2b375b29bd4f9adb4095fa303d55901f9a82
Dmytro-Hrebenyuk <149362391+Dmytro-Hrebenyuk@users.noreply.github.com>
2024-11-03 16:41:35 +0200

    Update DHLDeliveryStrategy.java

--- src/main/java/flower/store/DHLDeliveryStrategy.java
+++ src/main/java/flower/store/DHLDeliveryStrategy.java
@@ -2,11 +2,37 @@ package flower.store;
 
 import java.util.List;
 
-public interface Delivery {
-    String deliver(List<Flower> flowers);
-    Order getOrder();
-    void setOrder(Order order);
-    String getAddress();
-    void setAddress(String address);
-    String getdelivery();
+public class DHLDeliveryStrategy implements Delivery {
+    private String adress;
+    private  Order order;
+    @Override
+    public String deliver(List<Flower> flowers) {
+        return "Flowers delivered via DHL: " + flowers.size() + " items.";
+    }
+    
+    @Override
+    public Order getOrder() {
+        return order;
+    }
+
+    @Override
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public String getAddress() {
+        return this.adress;
+    }
+
+    @Override
+    public void setAddress(String address) {
+        this.adress = address;
+    }
+
+    @Override
+    public String getdelivery() {
+        return "DHL Delivery";
+    }
+    
 }
